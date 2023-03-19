51f28d330c33301e4e327f626334f9ca0846d6c6
Rohan <rohanb158@gmail.com>
2023-03-19 17:55:50 +0530

    used some validator from express validatos

--- backend/routes/createuser.js
+++ backend/routes/createuser.js
@@ -1,17 +1,29 @@
 const express = require("express")
 const router = express.Router()
 const user = require("../model/User")
+const { body, validationResult } = require('express-validator');
 router.get('/data', async (req ,res)=>{
     res.json({success:true});
     
 })
-router.post('/createUser' , async (req ,res)=>{
-  try{
+router.post('/createUser' , 
+ // username must be an email
+ body('email',"invalid email").isEmail(),
+ body('name', "invalid name").isLength({ min: 5 }),
+ 
+ body('password',"invalid password").isLength({ min: 5 }),
+async (req ,res)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ flaws: errors.array() });
+    }
+  
+    try{
     await user.create({
-        name:"rohan",
-        location:"bhopal",
-        email:"rohan1@gmail.com",
-        password:"1234"
+        name:req.body.name,
+        location:req.body.location,
+        email:req.body.email,
+        password:req.body.password
     })
     res.json({success:true});
 }catch(err){

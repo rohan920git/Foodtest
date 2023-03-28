338e138456a596517ef03797a5f997bf4edc8ce5
Rohan <rohanb158@gmail.com>
2023-03-28 22:21:47 +0530

    completed signup and login

--- backend/routes/createuser.js
+++ backend/routes/createuser.js
@@ -2,29 +2,64 @@ const express = require("express");
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
 const User = require("../model/users");
-router.post("/createUser"
-,[
-   body("name").isLength({min:2}),
-   body("email").isEmail(),
-   body("password").isLength({min:6})
-]
-, async (req , res)=>{
-   const errors = validationResult(req);
-   if (!errors.isEmpty()) {
-     return res.status(400).json({ errors: errors.array() });
-   }
- try{ 
-   console.log(req.body.name , req.body.email)
-     await User.create({
-        name:req.body.name,
-        email:req.body.email,
-        password:req.body.password
-     })
-     res.json({success:true}) 
- }
- catch(error){
-   console.log(error)
-   res.json({success:false}) 
-}
-})
+  router.post("/createUser"
+  ,[
+    body("name").isLength({min:2}),
+    body("email").isEmail(),
+    body("password").isLength({min:6})
+  ]
+  , async (req , res)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+  try{ 
+    console.log(req.body.name , req.body.email)
+      await User.create({
+          name:req.body.name,
+          email:req.body.email,
+          password:req.body.password
+      })
+      res.json({success:true}) 
+  }
+  catch(error){
+    console.log(error)
+    res.json({success:false}) 
+  }
+  })
+  router.post("/loginUser"
+  ,[
+ 
+    body("email").isEmail(),
+    body("password").isLength({min:6})
+  ]
+  , async (req , res)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    let email = req.body.email;
+  try{ 
+   
+      let userdata = await User.findOne({email})
+      if(!userdata){
+        return res.status(400).json({error : "enter correct email",
+                                    Message:"enter  valid email" ,
+                                  success:false}
+                                    );
+      } 
+      if(! (req.body.password === userdata.password)){
+        
+        return res.status(400).json({error : "enter correct password" ,
+                                      Message : "password is worong",
+                                      success:false});
+      }
+           
+      return res.json({success:true});
+  }
+  catch(error){
+    console.log(error)
+    res.json({success:false}) 
+  }
+  })
 module.exports =router;
--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -1,10 +1,66 @@
 import React from 'react'
+import { useState } from 'react';
+import { Link , useNavigate} from 'react-router-dom';
 
 function Login() {
+  let nevigate = useNavigate();
+  const [message, setmessage] = useState("enter your email");
+  const messageupdate=(json)=>{
+    setmessage(json.Message);
+  }
+  const [credential , setcredential] = useState({email:"",password:""})
+        const changehandler = (e) =>{
+               setcredential({...credential, [e.target.name]: e.target.value});
+        }   
+      const handlesub = async (e)=>{
+         e.preventDefault();
+         const response = await fetch("http://localhost:5000/api/loginUser",{
+          method: 'POST', // Using POST request to create a new resource in the database
+          mode: 'cors', // no-cors, cors, *same-origin
+          cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+          credentials: 'same-origin', // include, *same-origin, omit
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          redirect: 'follow', // manual, *follow, error
+          referrer: 'no-referrer', 
+              body:JSON.stringify({email:credential.email,password:credential.password})
+              
+         })
+         const json = await response.json();
+         console.log(json);
+         if(!json.success){
+           alert("enter valid credentials")
+          }
+         if(json.success){
+            nevigate("/")
+          }
+         messageupdate(json);
+    }
   return (
-    <div>
-      <h2>login now</h2>
-    </div>
+    <>
+     <div className="container">
+     <form onSubmit={handlesub} multipart="urlencoded">
+  
+ 
+  <div className="mb-3">
+    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email" value={credential.email} onChange={changehandler}/>
+    <div id="emailHelp" className="form-text">{message}</div>
+  </div>
+  <div className="mb-3">
+    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+    <input type="password" className="form-control" id="exampleInputPassword1" name="password" value={credential.password} onChange={changehandler}/>
+  </div>
+  
+  <button type="submit" className="m-3 btn btn-success">Login</button>
+  <Link to={"/createuser"} className="m-3 btn btn-danger" >Sign-Up</Link>
+</form>
+</div>
+
+    </>
+      
+
   )
 }
 
